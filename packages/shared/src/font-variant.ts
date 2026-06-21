b803e5c5debb750fe046e41d81f2546783be8464
Pongsakorn Thipayanate <166195305+pyyupsk@users.noreply.github.com>
2026-06-21 15:02:23 +0700

    chore: remove unused packages/shared

--- packages/shared/src/font-variant.ts
+++
@@ -1,9 +0,0 @@
-export type FontStyle = "normal" | "italic";
-
-export interface FontVariant {
-  id: string;
-  familyId: string;
-  style: FontStyle;
-  weight: number;
-  postScriptName: string;
-}
--- packages/shared/src/subset.ts
+++
@@ -1,4 +0,0 @@
-export interface Subset {
-  id: string;
-  name: string;
-}
